bea2800840bb56b0e2f9e4d60b611f98aeef0f8c
daonham <daonham95@gmail.com>
2024-11-27 22:32:07 +0700

    fix: next-auth

--- apps/web/lib/auth/auth.config.ts
+++ apps/web/lib/auth/auth.config.ts
@@ -1,61 +1,7 @@
-import { type NextAuthConfig, AuthError } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import Google from 'next-auth/providers/google';
+import { type NextAuthConfig } from 'next-auth';
 
 export default {
-  providers: [
-    Google({
-      allowDangerousEmailAccountLinking: true
-    }),
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { type: 'email' },
-        password: { type: 'password' }
-      },
-      async authorize(credentials) {
-        const { email, password } = credentials;
-
-        if (!email || !password) {
-          throw new Error('no-credentials');
-        }
-
-        const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/login`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ email, password })
-        });
-
-        if (!response.ok) {
-          throw new AuthError('User not found');
-        }
-
-        const user = await response.json();
-
-        // refactor: code
-        if (user?.error) {
-          if (user.error === 'invalid-credentials') {
-            throw new AuthError('Please provide an email and password.');
-          }
-
-          if (user.error === 'email-not-verified') {
-            throw new AuthError('Please verify your email address.');
-          }
-
-          throw new AuthError(user.error);
-        }
-
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          image: user?.image || ''
-        };
-      }
-    })
-  ],
+  providers: [],
   pages: {
     signIn: '/login',
     error: '/login'
--- apps/web/lib/auth/index.ts
+++ apps/web/lib/auth/index.ts
@@ -1,6 +1,9 @@
+import { verifyPasswordHash } from '@/lib/auth/password';
 import client from '@/lib/db';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
-import NextAuth from 'next-auth';
+import NextAuth, { AuthError } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import Google from 'next-auth/providers/google';
 import { cache } from 'react';
 import authConfig from './auth.config';
 
@@ -14,7 +17,49 @@ const {
     databaseName: process.env.MONGODB_DB_NAME
   }),
   session: { strategy: 'jwt' },
-  ...authConfig
+  ...authConfig,
+  providers: [
+    Google({
+      allowDangerousEmailAccountLinking: true
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { type: 'email' },
+        password: { type: 'password' }
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials as any;
+
+        if (!email || !password) {
+          throw new AuthError('Email and password are required');
+        }
+
+        const user = await client.db(process.env.MONGODB_DB_NAME).collection('users').findOne({ email: email });
+
+        if (!user || !user.password) {
+          throw new AuthError('Please provide an email and password.');
+        }
+
+        const passwordMatch = verifyPasswordHash(user.password, password);
+
+        if (!passwordMatch) {
+          throw new AuthError('Please provide an email and password.');
+        }
+
+        if (!user.emailVerified) {
+          throw new AuthError('Please verify your email address.');
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          image: user?.image || ''
+        };
+      }
+    })
+  ]
 });
 
 const auth = cache(uncachedAuth);
