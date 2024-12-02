de9f1e35854c9f62f33ae5fe95c6f6a7fde3251e
daonham <daonham95@gmail.com>
2024-12-02 13:48:34 +0700

    fix: some ui

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -17,7 +17,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
         <input
           type={isPasswordVisible ? 'text' : type}
           className={cn(
-            'w-full max-w-md rounded-lg border border-gray-300 text-gray-900 placeholder-gray-400 focus:border-gray-600 focus:outline-none focus:ring-gray-600 sm:text-sm',
+            'w-full max-w-md rounded-md border border-gray-300 text-gray-900 placeholder-gray-400 focus:border-gray-600 focus:outline-none focus:ring-gray-600 sm:text-sm',
             props.error && 'border-red-500 focus:border-red-500 focus:ring-red-500',
             className
           )}
