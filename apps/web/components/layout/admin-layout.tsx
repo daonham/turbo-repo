8ec9cb4e671496237250d65c33dca5efefe0fc06
daonham <daonham95@gmail.com>
2024-12-11 14:08:58 +0700

    fix: admin layout

--- apps/web/components/layout/admin-layout.tsx
+++ apps/web/components/layout/admin-layout.tsx
@@ -374,8 +374,13 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
       </Sidebar>
 
       <SidebarInset>
-        <SidebarTrigger />
-        {children}
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger />
+            <div className="mr-2 h-4 w-[1px] shrink-0 bg-gray-200"></div>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
       </SidebarInset>
     </SidebarProvider>
   );
