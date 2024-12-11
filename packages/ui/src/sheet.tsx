985f92deb6b8bc46d79a44f7529e56f029700691
daonham <daonham95@gmail.com>
2024-12-11 11:37:37 +0700

    fix: sheet UI

--- packages/ui/src/sheet.tsx
+++ packages/ui/src/sheet.tsx
@@ -27,7 +27,7 @@ const SheetOverlay: React.FC<React.ComponentProps<typeof SheetPrimitive.Overlay>
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-  'fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
+  'fixed z-50 gap-4 bg-white p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-200 data-[state=open]:duration-400',
   {
     variants: {
       side: {
@@ -54,7 +54,7 @@ const SheetContent: React.FC<React.ComponentProps<typeof SheetPrimitive.Content>
     <SheetOverlay />
     <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
       {children}
-      <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-gray-100">
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
@@ -74,12 +74,12 @@ const SheetFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElemen
 SheetFooter.displayName = 'SheetFooter';
 
 const SheetTitle: React.FC<React.ComponentProps<typeof SheetPrimitive.Title>> = ({ ref, className, ...props }) => (
-  <SheetPrimitive.Title ref={ref} className={cn('text-foreground text-lg font-semibold', className)} {...props} />
+  <SheetPrimitive.Title ref={ref} className={cn('text-lg font-semibold text-gray-800', className)} {...props} />
 );
 SheetTitle.displayName = SheetPrimitive.Title.displayName;
 
 const SheetDescription: React.FC<React.ComponentProps<typeof SheetPrimitive.Description>> = ({ ref, className, ...props }) => (
-  <SheetPrimitive.Description ref={ref} className={cn('text-muted-foreground text-sm', className)} {...props} />
+  <SheetPrimitive.Description ref={ref} className={cn('text-sm text-gray-600', className)} {...props} />
 );
 SheetDescription.displayName = SheetPrimitive.Description.displayName;
 
