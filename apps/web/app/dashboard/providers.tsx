fabfaf9db45599bd50933bce9653d6f2a61dd934
daonham <daonham95@gmail.com>
2024-12-12 09:12:44 +0700

    fix: provider

--- apps/web/app/dashboard/providers.tsx
+++ apps/web/app/dashboard/providers.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
-import { ReactNode } from 'react';
 
-export default function Providers({ session, children }: { session: any; children: ReactNode }) {
+export default function Providers({ session, children }: { session: any; children: React.ReactNode }) {
   return (
     <SessionProvider session={session} key={session?.user.id}>
       {children}
