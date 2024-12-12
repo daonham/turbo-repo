b576267c3c66894909ceb1b1fa27de7b72c12123
daonham <daonham95@gmail.com>
2024-12-12 09:09:44 +0700

    feat: add toast color icon

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { TooltipProvider } from '@repo/ui';
+import { CircleAlert, CircleCheck, Info, TriangleAlert } from 'lucide-react';
 import { ReactNode } from 'react';
 import { Toaster } from 'sonner';
 
@@ -8,7 +9,17 @@ export default function RootProviders({ children }: { children: ReactNode }) {
   return (
     <TooltipProvider>
       {children}
-      <Toaster closeButton position="bottom-center" className="pointer-events-auto" />
+      <Toaster
+        closeButton
+        position="bottom-center"
+        className="pointer-events-auto"
+        icons={{
+          error: <CircleAlert className="size-5 text-red-500" />,
+          success: <CircleCheck className="size-5 text-emerald-500" />,
+          info: <Info className="size-5 text-blue-500" />,
+          warning: <TriangleAlert className="size-5 text-amber-500" />
+        }}
+      />
     </TooltipProvider>
   );
 }
