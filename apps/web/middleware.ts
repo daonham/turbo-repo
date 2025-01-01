a26d2f3116a2ce029ef62d9bbda4ab41980a23de
daonham <daonham95@gmail.com>
2025-01-01 22:26:42 +0700

    refactor: middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,33 +1,34 @@
-import type { auth } from '@/lib/auth';
 import { NextResponse, type NextRequest } from 'next/server';
-
-type Session = typeof auth.$Infer.Session;
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const isAuthPage = ['/login', '/register', '/forgot-password', '/reset-password'].some((p) => path.startsWith(p));
   const isDashboardPage = path.startsWith('/dashboard');
 
-  const res = await fetch(`${req.nextUrl.origin}/api/auth/get-session`, {
-    headers: {
-      cookie: req.headers.get('cookie') || ''
-    }
-  });
+  if (isAuthPage || isDashboardPage) {
+    const fetchSession = await fetch(`${req.nextUrl.origin}/api/auth/get-session`, {
+      headers: {
+        cookie: req.headers.get('cookie') || ''
+      }
+    });
 
-  const isAuth = await res.json();
+    const isAuth = await fetchSession.json();
 
-  if (isAuth && isAuthPage) {
-    return NextResponse.redirect(new URL('/dashboard', req.url));
-  }
-
-  if (!isAuth && isDashboardPage) {
-    let from = path;
-
-    if (req.nextUrl.search) {
-      from += req.nextUrl.search;
+    if (isAuth && isAuthPage) {
+      return NextResponse.redirect(new URL('/dashboard', req.url));
     }
 
-    return NextResponse.redirect(new URL(`/login?from=${encodeURIComponent(from)}`, req.url));
+    if (!isAuth && isDashboardPage) {
+      let from = path;
+
+      if (req.nextUrl.search) {
+        from += req.nextUrl.search;
+      }
+
+      return NextResponse.redirect(new URL(`/login?from=${encodeURIComponent(from)}`, req.url));
+    }
+
+    return NextResponse.next();
   }
 
   return NextResponse.next();
