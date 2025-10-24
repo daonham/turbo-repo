4c63fd425148dc71aba9ce032a585099fe56b9eb
Joyce <daonham95@gmail.com>
2025-10-24 14:26:24 +0700

    style: update NotFound component for improved layout and accessibility, adjusting text sizes and link styles

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -3,16 +3,19 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className="relative flex min-h-svh w-full flex-col items-center justify-center gap-4">
-      <div className="text-2xl font-bold text-neutral-200">404</div>
-      <h1 className="font-display bg-gradient-to-r from-black to-neutral-600 bg-clip-text text-5xl font-semibold leading-normal text-transparent">
+      <div className="text-5xl font-semibold text-neutral-200">404</div>
+      <h1 className="font-display bg-gradient-to-r from-black to-neutral-600 bg-clip-text text-2xl font-semibold leading-normal text-transparent">
         Page not found
       </h1>
       <p className="text-base text-neutral-500">The page you are looking for does not exist.</p>
-      <div className="mt-6 flex gap-2">
+      <div className="mt-3 flex gap-2">
         <Link href="/" className="rounded-lg bg-gray-800 px-3 py-2 text-sm font-medium text-white">
           Go to home?
         </Link>
-        <Link href="/support" className="rounded-lg px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-100 hover:text-gray-900">
+        <Link
+          href="/support"
+          className="boder-1 rounded-lg border border-gray-200 px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-100 hover:text-gray-900"
+        >
           Contact support
         </Link>
       </div>
